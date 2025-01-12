bede5c324ca6b5bcd8fc7f8bd523649d925cad24
cbozic <cbozic@gmail.com>
2025-01-12 13:52:03 +0000

    updated gitignore

--- servicedisplay/src/components/VideoFrame.js
+++ servicedisplay/src/components/VideoFrame.js
@@ -3,7 +3,7 @@ import './VideoFrame.css';
 function VideoFrame() {
   return (
       <iframe className="VideoFrame"
-          src="https://www.youtube.com/embed/7GtdOFQsYyU?si=HWFMAn2cmtZ8QoPn" 
+          src="https://www.youtube.com/embed/VkIrcKoA98A?" 
           title="YouTube video player" 
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
