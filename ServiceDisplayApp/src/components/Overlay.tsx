236ba394a51d68d65f401a79692fc7e4df706658
Chris Bozic <cbozic@gmail.com>
2025-01-19 22:48:37 -0500

    Added an ios prototype

---
+++ ServiceDisplayApp/src/components/Overlay.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Overlay.css';
+
+interface OverlayProps {
+    showOverlay: boolean;
+    slide?: string;
+}
+
+function Overlay(props: OverlayProps) {  
+    return (
+        <div className={props.showOverlay?"overlay fadeOut":"overlay fadeIn"}>
+            {props.slide? <img src={props.slide} height="99%" width="99%" alt="Overlay Slide" />: <div/>}
+        </div>
+    );
+}
+
+export default Overlay;
