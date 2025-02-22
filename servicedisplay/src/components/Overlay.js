41ae32dd2bc566c3bd278ed4867588295a66c6cc
Chris Bozic <cbozic@gmail.com>
2025-02-22 16:37:23 -0500

    Dynamic code for Overlay animation rather than CSS

--- servicedisplay/src/components/Overlay.js
+++ servicedisplay/src/components/Overlay.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import './Overlay.css';
 
-function Overlay(props) {  
+function Overlay(props) {
     return (
-        <div className={props.showOverlay?"overlay fadeOut":"overlay fadeIn"}>
-            {props.slide? <img src={props.slide} height="99%" width="99%" alt="Overlay Slide" />: <div/>}
+        <div style={{
+            animation: props.showOverlay ? "fadeOut 2s" : "fadeIn 2s",
+            opacity: props.showOverlay ? "1" : "0",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            backgroundColor: "rgba(0, 0, 0)", /* black */
+            pointerEvents: "all",
+        }}>
+            {props.slide ? <img src={props.slide} height="99%" width="99%" alt="Overlay Slide" /> : <div />}
         </div>
     );
 }
