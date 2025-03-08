ef8624889091d0dd14cdb94fc4e4756d60c9e635
Chris Bozic <cbozic@gmail.com>
2025-03-08 10:58:13 -0500

    blocked all mouse interaction with youtube again

--- servicedisplay/src/components/VideoFadeFrame.tsx
+++ servicedisplay/src/components/VideoFadeFrame.tsx
@@ -407,6 +407,14 @@ const VideoFadeFrame: React.FC<VideoFadeFrameProps> = ({
             onReady={onPlayerReadyHandler} 
             onStateChange={onStateChangeHandler} 
           />
+          <div style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+            zIndex: 10,
+          }} />
         </div>
       </div>
       {useOverlay && !isUnderlayMode && (
